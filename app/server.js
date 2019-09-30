f5b8726be3b611dc858a91075c258720eb80b9e1
Anumita Shenoy <ansheno@microsoft.com>
2019-09-30 17:18:48 +0530

    Added deployment files

--- app/server.js
+++ app/server.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const express = require('express');
+const appInsights = require("applicationinsights");
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
+appInsights.setup("83eba91c-9953-4fbf-8267-cad4d4d1f397");
+appInsights.start();
 
 const app = express();
 app.get('/', (req, res) => {
