273620094f01567b5aeb9f87d65036ac18dcd595
Anumita Shenoy <ansheno@microsoft.com>
2019-09-30 20:15:17 +0530

    Added to namespace nodejs

--- app/server.js
+++ app/server.js
@@ -2,15 +2,29 @@
 
 const express = require('express');
 const appInsights = require("applicationinsights");
-
 const PORT = 8080;
 const HOST = '0.0.0.0';
+
 appInsights.setup("83eba91c-9953-4fbf-8267-cad4d4d1f397");
 appInsights.start();
+let client = appInsights.defaultClient;
+
+function randomInt(low, high) {
+  return Math.floor(Math.random() * (high - low) + low)
+}
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  if (randomInt(1, 100) > 30)
+  {
+    client.trackMetric({name: "custom_status", value: "bad"});
+    res.status(500).send('Internal Error\n');
+  }
+  else
+  {
+    client.trackMetric({name: "custom_status", value: "good"});
+    res.status(200).send('Hello world\n');
+  }
 });
 
 app.listen(PORT, HOST);
